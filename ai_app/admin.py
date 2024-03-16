addfaa4d254316826c26d7bded76665492ffafc3
Łukasz 69939 Merito <gax34645@student.gdynia.merito.pl>
2024-03-16 11:09:43 +0100

    podłączenie modelu, wykonanie migracji

--- ai_app/admin.py
+++ ai_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import ImageElement
 # Register your models here.
+admin.site.register(ImageElement)
--- ai_app/models.py
+++ ai_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ImageElement(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="mediaphoto", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
